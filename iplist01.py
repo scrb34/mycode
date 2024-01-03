d358f9a165b5474f2340eb038c50873015af4cc9
scrb34 <scrb43@icloud.com>
2024-01-03 18:05:31 +0000

    mixed lists

---
+++ iplist01.py
@@ -0,0 +1,5 @@
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 'f-string'
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
